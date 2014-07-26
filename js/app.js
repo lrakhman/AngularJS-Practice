493ecc5cd1bacdd5032c671987b0d66fdfc7954c
lrakhman <lana.rakhman@gmail.com>
2014-07-26 10:58:23 -0500

    set up initial file structure

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var triviaApp = angular.module('triviaApp', []);
+
+triviaApp.controller('QuestionListCtrl', function($scope) {
+	$scope.questions = [
+		{
+			id: 1,
+			question: "Why is AngularJS named AngularJS?",
+			option1: "It sounds cool.",
+			option2: "It describes HTML brackets.",
+			option3: "No one ones."
+			answer: "2"
+		}, 
+
+		{
+			id: 2,
+			question: "Why does AngularJS use the namespace ng?",
+			option1: "It stands for n gage (engage).",
+			option2: "It's the initials of the original author.",
+			option3: "It sounds like angular."
+			answer: "3"
+		}
+	]
+});
